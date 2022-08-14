a51e924f53850b694794b950f81aa886e0fa1fed
cacing69 <ibnuul@gmail.com>
2022-08-15 01:09:33 +0700

    change func name

--- api/index.go
+++ api/index.go
@@ -1,26 +1,18 @@
 package handler
 
-// import (
-// 	"net/http"
-// 	"github.com/labstack/echo/v4"
-// )
-
-// func Api() {
-// 	e := echo.New()
-// 	e.GET("/api/", func(c echo.Context) error {
-// 		return c.String(http.StatusOK, "Hello, World!")
-// 	})
-// 	e.Logger.Fatal(e.Start(":1323"))
-// }
-// package handler
-
 import (
 	"net/http"
 	"fmt"
-	// "github.com/labstack/echo/v4"
+  "github.com/gorilla/mux"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-  // e := echo.New()
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	log.Println(*r)
+	w.Write("lorem")
+}
+
+func GetRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", Handler).Methods(http.MethodGet)
+	return r
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/cacing69/go-echo
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.8.0
+require github.com/gorilla/mux v1.8.0 // indirect
