337e83cc3b417db6076fd348312a9d58c52d1c52
cacing69 <ibnuul@gmail.com>
2022-08-15 00:13:31 +0700

    chdir

--- api/main.go
+++ api/main.go
@@ -1,11 +1,11 @@
-package api
+package main
 
 import (
 	"net/http"
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
+func Index() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
