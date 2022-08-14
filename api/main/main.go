6852e7f4efa4d97b8a37c5e2e2291742a97e89b4
cacing69 <ibnuul@gmail.com>
2022-08-15 00:31:37 +0700

    change func name

--- api/main/main.go
+++ api/main/main.go
@@ -1,11 +1,11 @@
-package handler
+package api
 
 import (
 	"net/http"
 	"github.com/labstack/echo/v4"
 )
 
-func Handler() {
+func API() {
 	e := echo.New()
 	e.GET("/api/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
